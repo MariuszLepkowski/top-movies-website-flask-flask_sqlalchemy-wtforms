7bb93b6dd436fd3a6224fae2279219a7e777208e
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-07-31 17:32:21 +0200

    Create new repository

---
+++ main.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+Bootstrap5(app)
+
+
+##CREATE DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///favorite-movies.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+
+##CREATE TABLE
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+## After adding the new_movie the code needs to be commented out/deleted.
+## So you are not trying to add the same movie twice. The db will reject non-unique movie titles.
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+# second_movie = Movie(
+#     title="Avatar The Way of Water",
+#     year=2022,
+#     description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
+#     rating=7.3,
+#     ranking=9,
+#     review="I liked the water.",
+#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
+# )
+#
+# with app.app_context():
+#     db.session.add(second_movie)
+#     db.session.commit()
+
+
+# third_movie = Movie(
+#     title="Drive",
+#     year=2011,
+#     description="A mysterious Hollywood stuntman and mechanic moonlights as a getaway driver and finds himself in trouble when he helps out his neighbor in this action drama.",
+#     rating=7.5,
+#     ranking=1,
+#     review="Loved it!""I liked the water.",
+#     img_url="https://www.shortlist.com/media/images/2019/05/the-30-coolest-alternative-movie-posters-ever-2-1556670563-K61a-column-width-inline.jpg"
+# )
+#
+# with app.app_context():
+#     db.session.add(third_movie)
+#     db.session.commit()
+
+# CREATE A FORM
+
+class UpdateForm(FlaskForm):
+    rating = StringField(
+        label="Your rating out of 10 e. g. 7.5",
+        validators=[DataRequired()],
+    )
+
+    review = StringField(
+        label="Your review",
+        validators=[DataRequired()],
+    )
+
+    submit = SubmitField(label="Done")
+
+
+@app.route("/")
+def home():
+    movies = db.session.execute(db.select(Movie).order_by(Movie.ranking)).scalars()
+    movies_sorted_by_ranking = sorted(movies, key=lambda x: x.ranking, reverse=True)
+    return render_template("index.html", movies=movies_sorted_by_ranking)
+
+
+@app.route("/edit", methods=['POST', 'GET'])
+def edit_rating_review():
+    movie_id = request.args.get('id')
+    movie_selected = db.get_or_404(Movie, movie_id)
+    form = UpdateForm()
+
+    if request.method == 'POST':
+        movie_selected.rating = request.form["rating"]
+        movie_selected.review = request.form["review"]
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("edit.html", form=form, movie=movie_selected)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
