e3e8b477120a88a3976ddb59e8f6d12b36c3c76a
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-08-01 16:44:38 +0200

    Change inside add_movie()

--- main.py
+++ main.py
@@ -139,8 +139,7 @@ def delete_movie():
 @app.route("/add", methods=['GET', 'POST'])
 def add_movie():
     form = AddMovie()
-    if request.method == 'POST':
-        return redirect(url_for('home'))
+
     return render_template("add.html", form=form)
 
 
